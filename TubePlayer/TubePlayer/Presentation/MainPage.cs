8cb75eaf1a299f1fd65ac02007c6405225b2bcdc
Steve Bilogan <steve.bilogan@gmail.com>
2023-11-07 09:15:53 -0500

    create basic ui

--- TubePlayer/TubePlayer/Presentation/MainPage.cs
+++ TubePlayer/TubePlayer/Presentation/MainPage.cs
@@ -4,28 +4,22 @@ namespace TubePlayer.Presentation
 	{
 		public MainPage()
 		{
-			this.DataContext<BindableMainModel>((page, vm) => page
-				.NavigationCacheMode(NavigationCacheMode.Required)
+			this.NavigationCacheMode(NavigationCacheMode.Required)
 				.Background(Theme.Brushes.Background.Default)
-				.Content(new Grid()
+				.Content(
+					new Grid()
 					.SafeArea(SafeArea.InsetMask.All)
-					.RowDefinitions<Grid>("Auto,*")
+					.RowDefinitions("Auto, *")
 					.Children(
-						new NavigationBar().Content(() => vm.Title),
-						new StackPanel()
+						new TextBox()
+							.PlaceholderText("Search term"),
+						new ListView()
 							.Grid(row: 1)
-							.HorizontalAlignment(HorizontalAlignment.Center)
-							.VerticalAlignment(VerticalAlignment.Center)
-							.Spacing(16)
-							.Children(
-								new TextBox()
-									.Text(x => x.Bind(() => vm.Name).Mode(BindingMode.TwoWay))
-									.PlaceholderText("Enter your name:"),
-								new Button()
-									.Content("Go to Second Page")
-									.AutomationProperties(automationId: "SecondPageButton")
-									.Command(() => vm.GoToSecond)
-									))));
+							.ItemsSource(new[] { "Avatar", "Titanic", "Star Wars" })
+							.ItemTemplate<string>(videoTitle =>
+								new TextBlock()
+									.Text(() => videoTitle))));
+
 		}
 	}
 }
