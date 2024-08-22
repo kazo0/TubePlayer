a5969f0e01a2a3bbe20fe31e098809157ea447ec
Steve Bilogan <steve.bilogan@gmail.com>
2024-08-21 21:48:46 -0400

    Add project files.

---
+++ TubePlayer/Presentation/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TubePlayer.Presentation;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ TubePlayer/Presentation/SecondModel.cs
@@ -0,0 +1,5 @@
+namespace TubePlayer.Presentation;
+
+public partial record SecondModel(Entity Entity)
+{
+}
